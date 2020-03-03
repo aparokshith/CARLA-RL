54fc98b6baf215b5442c17f1ab8108dc04bdf974
aparokshith <aparokshith1997@gmail.com>
2020-03-02 20:12:16 -0700

    added video sensor to the car

--- main.py
+++ main.py
@@ -22,6 +22,19 @@ import carla
 
 import random
 import time
+import numpy as np
+import cv2
+
+IM_WIDTH = 640
+IM_HEIGHT = 480
+
+def process_img(image):
+    i = np.array(image.raw_data)
+    i2 = i.reshape(IM_HEIGHT,IM_WIDTH,4)
+    i3 = i2[:,:,:3]
+    cv2.imshow("",i3)
+    cv2.waitKey(1)
+    return i3/255.0
 #%%
 actor_list = []
 
@@ -42,8 +55,22 @@ try:
     
     actor_list.append(vehicle)
     
+    cam_bp = blueprint_library.find("sensor.camera.rgb")
+    
+    cam_bp.set_attribute("image_size_x",f"{IM_WIDTH}")
+    cam_bp.set_attribute("image_size_y",f"{IM_HEIGHT}")
+    cam_bp.set_attribute("fov","110")
+    
+    spawn_point = carla.Transform(carla.Location(x = 2.5,z = 0.7))
+    sensor = world.spawn_actor(cam_bp,spawn_point,attach_to = vehicle)
+    actor_list.append(sensor)
+    
+    sensor.listen(lambda data:process_img(data))
+    
+    
     time.sleep(10)
 finally:
     for actor in actor_list:
         actor.destroy()
+        cv2.destroyAllWindows()
         print("Cleared up")
